ab76a02655773716c8ef2e0336092ef3b46ff40d
Jason Crawford <daxapps777@gmail.com>
2017-11-22 13:30:08 -0600

    NavBar styling

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TabNavigator, StackNavigator } from "react-navigation";
 
-import AuthScreen from './screens/AuthScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import MapScreen from './screens/MapScreen';
-import DeckScreen from './screens/DeckScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import ReviewScreen from './screens/ReviewScreen';
-
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
       auth: { screen: AuthScreen },
-      main: { 
+      main: {
         screen: TabNavigator({
           map: { screen: MapScreen },
           deck: { screen: DeckScreen },
@@ -29,17 +28,15 @@ export default class App extends React.Component {
       }
     });
 
-    return (
-      <MainNavigator />
-    );
+    return <MainNavigator />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
+	static navigationOptions = props => {
+		const { navigation } = props;
+		const { navigate } = navigation;
+		return {
+			headerTitle: "Review Jobs",
+			headerRight: (
+				<Button
+					title="Settings"
+					onPress={() => navigate("settings")}
+					backgroundColor="rgba(0,0,0,0)"
+					color="rgba(0, 125, 125, 1)"
+				/>
+			)
+		};
+	};
+
 	render() {
 		return (
 			<View>
