f69e1adb79970e28f22bdc3d6ab290f1568bfddb
Jason Crawford <daxapps777@gmail.com>
2017-11-24 09:20:07 -0600

    Setup Redux

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import Expo from "expo";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TabNavigator, StackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import AuthScreen from "./screens/AuthScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import MapScreen from "./screens/MapScreen";
@@ -28,7 +31,11 @@ export default class App extends React.Component {
       }
     });
 
-    return <MainNavigator />;
+    return ( 
+      <Provider store={store}>
+        <MainNavigator />
+      </Provider>
+    );
   }
 }
 
