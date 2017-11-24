54de7b3a0e7e34d6d76bf62cb643c6dbf8091f3f
Jason Crawford <daxapps777@gmail.com>
2017-11-24 12:27:34 -0600

    Advance to Map Screen if logged in

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,19 +6,28 @@ import * as actions from "../actions";
 class AuthScreen extends Component {
 	componentDidMount() {
 		this.props.facebookLogin();
+		this.onAuthComplete(this.props);
 	}
+
+	componentWillReceiveProps(nextProps) {
+		this.onAuthComplete(nextProps);
+	}
+
+	onAuthComplete(props) {
+		if (props.token) {
+			this.props.navigation.navigate('map');
+		}
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>AuthScreen</Text>
-				<Text>AuthScreen</Text>
-				<Text>AuthScreen</Text>
-				<Text>AuthScreen</Text>
-				<Text>AuthScreen</Text>
-				<Text>AuthScreen</Text>
-			</View>
+			<View />
 		);
 	}
+}
+
+function mapStateToProps({ auth }) {
+	return { token: auth.token };
 }
 
 export default connect(null, actions)(AuthScreen);
