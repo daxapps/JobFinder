6157750186eacdb5a205cc4223990f9b86fb55fb
Jason Crawford <daxapps777@gmail.com>
2017-11-25 10:00:24 -0600

    Initial MapView setup

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+	state = {
+		mapLoaded: false,
+		region: {
+			latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+		}
+	}
+
+	componentDidMount() {
+		this.setState({ mapLoaded: true });
+	}
+
 	render() {
+		if (!this.state.mapLoaded) {
+			return (
+				<View style={{ flex: 1, justifyContent: 'center'}}>
+					<ActivityIndicator size="large" />
+				</View>
+			);
+		}
+
 		return (
-			<View>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
+			<View style={{ flex: 1 }}>
+				<MapView 
+					region={this.state.region}
+					style={{ flex: 1 }} 
+				/>
 			</View>
 		);
 	}
